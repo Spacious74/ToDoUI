a3688a29e1ffa7b607ffb9f25a2bf71270051ba5
VishalS <vishalsavita809018@gmail.com>
2022-12-25 13:34:08 +0530

    Adding js

--- todo.js
+++ todo.js
@@ -7,14 +7,70 @@ if (taskTabContainer.childElementCount == 0) {
     document.querySelector(".emptyContainer").style.display = 'none';
 }
 
+let filter = document.getElementById("filter");
+filter.addEventListener("keyup", filterTodos)
+function filterTodos(e) {
+
+    //convert to lowercase
+
+    let searchText = filter.value.toLowerCase();
+    let allTodosList = document.querySelector(".tasktabcontainer").children;
+    
+    Array.from(allTodosList).forEach(item => {
+        let todoitem = item.firstChild.lastChild.value;
+        // console.log(todoitem.toLowerCase().indexOf(searchText));
+        if (todoitem.toLowerCase().indexOf(searchText) != -1) {
+            item.style.display = 'flex'
+
+        } else {
+            item.style.display = 'none'
+        }
+    })
+
+
+}
+
+
+
+
+
 function deleteme(n) {
     document.getElementById('tasktab' + n).remove();
     let element = document.getElementById("taskcontainer").childElementCount;
-    console.log(element);
     if(element == 0){
         document.querySelector(".emptyContainer").style.display = 'flex';
     }
     document.getElementById('taskcounter').innerHTML = element;
+}
+
+function editme(n){
+    let inputField = document.getElementById('edittext' + n);
+    inputField.removeAttribute('readonly');
+    inputField.style.backgroundColor = "white";
+    inputField.style.outline = "solid 1px grey";
+    inputField.style.borderRadius = "6px";
+    inputField.autofocus = "true";
+    let Edit = document.getElementById('editBtn' + n);
+    Edit.style.display = "none";
+    let Save = document.getElementById('saveBtn' + n);
+    Save.style.display = "block";
+}
+
+
+function saveme(n){
+    let editedText = document.getElementById('edittext' + n).value;
+    if(editedText.length == 0){
+        alert("Please enter a task");
+        return;
+    }
+    let inputField = document.getElementById('edittext' + n);
+    inputField.setAttribute('readonly', true);
+    inputField.style.backgroundColor = "transparent";
+    inputField.style.outline = "none";
+    let Edit = document.getElementById('editBtn' + n);
+    Edit.style.display = "block";
+    let Save = document.getElementById('saveBtn' + n);
+    Save.style.display = "none";
 }
 
 let t = 1;
@@ -30,12 +86,10 @@ function addtask() {
     taskTab.classList.add("taskstab");
     let id = "tasktab" + t;
     taskTab.setAttribute("id", id);
-    // taskTab.style.display = "flex";
     taskTabContainer.appendChild(taskTab);
 
     let taskcont = document.createElement('div');
-    taskcont.style.display = "flex";
-    taskcont.style.alignItems = "center";
+    taskcont.setAttribute("class", "checkandtext");
 
     // checkbox
     let check = document.createElement("input");
@@ -49,14 +103,25 @@ function addtask() {
     taskcont.appendChild(check);
 
     // text inside task tab
-    let textCont = document.createElement("div");
-    textCont.innerText = task;
+    let textCont = document.createElement("input");
+    textCont.value = task;
+    textCont.setAttribute("type","text");
     textCont.setAttribute("class","tasktext");
+    let textId = "edittext" + t;
+    textCont.setAttribute("id", textId);
+    textCont.setAttribute("readonly", true);
 
 
     taskcont.append(textCont);
 
-    // delete button inside task tab
+    // delete and edit button inside task tab
+
+    let btnContainer = document.createElement("div");
+    btnContainer.setAttribute("class","btns");
+    btnconId = "btncon" + t;
+    btnContainer.setAttribute("id", btnconId);
+
+    // delete button
     let dltbutton = document.createElement("button");
     dltbutton.classList.add("btn", "dltbtn");
     let buttonId = "deleteBtn" + t;
@@ -65,16 +130,39 @@ function addtask() {
     let fun = "deleteme(" + t + ")";
     dltbutton.setAttribute("onclick", fun);
 
+
+    // edit button 
+    let editbutton = document.createElement("button");
+    editbutton.classList.add("btn", "editbtn");
+    let editId = "editBtn" + t;
+    editbutton.setAttribute("id", editId);
+    editbutton.innerHTML = '<img src="https://img.icons8.com/ios-glyphs/24/ffffff/pencil--v1.png" class="delicon" style="margin-bottom: 5px;"/>';
+    let editfun = "editme(" + t + ")";
+    editbutton.setAttribute("onclick", editfun);
+
+    // savebutton
+    let savebutton = document.createElement("button");
+    savebutton.classList.add("btn", "savebtn");
+    let saveId = "saveBtn" + t;
+    savebutton.setAttribute("id", saveId);
+    savebutton.innerHTML = '<img src="https://img.icons8.com/ios-glyphs/24/ffffff/save--v1.png" class="delicon" style = "margin-bottom: 5px;"/>'
+    let savefun = "saveme(" + t + ")";
+    savebutton.setAttribute("onclick", savefun);
+    savebutton.style.display = "none";
+
+
+    btnContainer.append(editbutton,savebutton,dltbutton);
+
     // done element
     let doneElement = document.createElement("div");
     doneElement.classList.add("done");
     let doneId = "done" + t;
     doneElement.setAttribute("id", doneId);
-    doneElement.innerHTML = "Done ";
+    doneElement.innerHTML = "Done";
     doneElement.style.display = "none";
 
     t++;
-    taskTab.append(taskcont, doneElement, dltbutton);
+    taskTab.append(taskcont, doneElement, btnContainer);
     let totaltask = document.getElementById('taskcontainer').childElementCount;
     document.getElementById('taskcounter').innerHTML = totaltask;
 
@@ -86,15 +174,21 @@ function checkedOrNot(t) {
     // console.log(checkElement.checked);
     let status = document.getElementById("tasktab" + t);
     let deleteBtn = document.getElementById("deleteBtn" + t);
+    let editBtn = document.getElementById("editBtn" + t);
+    let btns = document.getElementById("btncon" + t);
     let done = document.getElementById("done" + t);
     if (checkElement.checked == true) {
         status.style.backgroundColor = "#c9ffc4";
         deleteBtn.style.display = "none";
+        editBtn.style.display = "none";
+        btns.style.display = "none";
         done.style.display = "block";
 
     } else {
         status.style.backgroundColor = "#eef1f5";
         deleteBtn.style.display = "block";
+        editBtn.style.display = "block";
+        btns.style.display = "flex";
         done.style.display = "none";
     }
 
